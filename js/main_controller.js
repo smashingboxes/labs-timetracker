b6dc164795ffa77f2131c8eadc0c54e1987d77c3
Rick Carlino <rick.carlino@gmail.com>
2014-09-26 11:45:27 -0500

    Minimally viable

--- js/main_controller.js
+++ js/main_controller.js
@@ -1,15 +1,26 @@
 var mainControllerFn = function($scope, $timeout, database){
   $scope.logs = [{text: 'nuthin!'}];
 
-  $scope.refresh = function(){
+  $scope.sync = function(){
     database.allDocs({include_docs: true}, function(err, response) {
       $scope.logs = response.rows;
+      $scope.$apply();
     });
   };
 
-  database.changes().on('change', function(change) {
-    $scope.refresh();
-  });
+  // // broke . . .
+  // database.changes().on('change', function (info) {
+  //   $scope.sync();
+  // }).on('created', function (info) {
+  //   // handle complete
+  //   $scope.sync();
+  // }).on('uptodate', function (info) {
+  //   // handle up-to-date
+  //   $scope.sync();
+  // }).on('error', function (err) {
+  //   alert("YOWZA!!!");
+  // });
+
 
   $scope.input = {
     text : '',
@@ -18,15 +29,19 @@ var mainControllerFn = function($scope, $timeout, database){
   };
 
   $scope.createLog = function(){
-    database.post({text: $scope.input.text})
-      .then(function(response) {
-        console.log(response);
-      });
+    database
+      .post({text: $scope.input.text})
+      .then(function(response) {$scope.sync();});
     $scope.input.text = '';
   };
 
+
+
   $scope.destroyLog = function(log) {
-    alert('not implemented. Ps: Alerts are awesome. ' + log.text);
+    database.remove(log.doc._id,
+                    log.doc._rev,
+                    {},
+                    function(err, response) { $scope.sync(); });
   }
 
   $scope.setReminder = function() {
@@ -39,6 +54,8 @@ var mainControllerFn = function($scope, $timeout, database){
     $scope.input.text = prompt('Hey- it looks like you havent checked in for a while. Mind telling me what you\'re doing?');
     $scope.createLog();
   };
+
+  $scope.sync()
 };
 
 var mainController = angular
